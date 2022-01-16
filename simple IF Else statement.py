4d9029a85d986137a1907dc49a2e14818429c9ba
WIlliam Dalton <36655180+Daltonws@users.noreply.github.com>
2022-01-16 09:39:13 -0500

    if-else simple statement

---
+++ simple IF Else statement.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+  print("You are tall enough to ride the rollercoaster!")
+else:
+  print("Sorry, you are NOT tall enough to ride the rollercoaster!")

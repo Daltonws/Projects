975b6f4365cdcecb4adb17fce94a614566004d88
WIlliam Dalton <36655180+Daltonws@users.noreply.github.com>
2022-01-18 14:54:45 -0500

    Create exerciseLIST.py

---
+++ exerciseLIST.py
@@ -0,0 +1,13 @@
+states_of_america = ["Alabama", "Alaska", "Arkansas"]
+print(states_of_america[0])
+print(states_of_america[-1])
+#make a chanhe to a list 
+states_of_america[1] = "Alaska1"
+print(states_of_america[1])
+print(states_of_america)
+#add a single item to the end of the list
+states_of_america.append("Connecticut")
+print(states_of_america)
+#add multiple items to a list
+states_of_america.extend(["Vermont", "Ohio"])
+print(states_of_america)

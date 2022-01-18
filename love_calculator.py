bff92e8ce3482a38f3e66b88d3cb2d758b34fa23
WIlliam Dalton <36655180+Daltonws@users.noreply.github.com>
2022-01-17 19:25:14 -0500

    Update love_calculator.py
    updating some variables

--- love_calculator.py
+++ love_calculator.py
@@ -1,9 +1,12 @@
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
+#need to use lower case for names to support better calculations
 name1 = str(name1.lower())
 name2 = str(name2.lower())
+#Need a catination here to create a single string of name char
 combined_names = name1 + name2
+#need to establish lower cases for static value "true love"
 lower_names = combined_names.lower()
 t = lower_names.count("t")
 r = lower_names.count("r")
@@ -16,20 +19,12 @@ o = lower_names.count("o")
 v = lower_names.count("v")
 e = lower_names.count("e")
 second_digit = l + o + v + e
+# You str unable to conduct equations or conditions on 2 different data type as seen on line # 29 so we need to turn the string into int.
+rating = int(str(first_digit) + str(second_digit))
 
-score = int(str(first_digit) + str(second_digit))
-
-if (score < 10) or (score > 90):
-  print(f"Your score is {score}, you go together like coke and mentos.")
-elif (score >= 40) and (score <= 50):
-  print(f"Your score is {score}, you are alright together.")
+if (rating < 10) or (rating > 90):
+  print(f"Your rating is {rating}, you go together like coke and mentos.")
+elif (rating >= 40) and (rating <= 50):
+  print(f"Your rating is {rating}, you are alright together.")
 else:
-  print(f"Your score is {score}.")
-
-
-
-# print(combined_names)
-# love_letters = ["t", "r", "u", "e", "l", "o", "v", "e"]
-# count = combined_names.count.str(love_letters)
-# print(combined_names)
-
+  print(f"Your rating is {rating}.")
